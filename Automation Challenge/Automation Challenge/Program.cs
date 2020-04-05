e5b3d7acc77282847b5784bcd713f1705e5d2e88
Christopher Aguilar <chrisaguilarenriquez@gmail.com>
2020-04-05 14:25:42 -0600

    Update

--- Automation Challenge/Automation Challenge/Program.cs
+++ Automation Challenge/Automation Challenge/Program.cs
@@ -18,7 +18,7 @@ namespace Automation_Challenge
             driver.Navigate().GoToUrl("http://www.shino.de/parkcalc/");
         }
 
-
+        
         [Test]
         public void ExecuteTest()
         {
@@ -70,15 +70,16 @@ namespace Automation_Challenge
             Assert.AreEqual(GivenResults, ExpectedResults);
 
         }
+        
 
         [Test]
         public void InitializeShortTerm()
         {
 
-            // Select Short Term Parking
+            // Select Long-Term Surface Parking
             IWebElement ParkingLotSelector = driver.FindElement(By.Id("ParkingLot"));
             var SelectParkingLot = new SelectElement(ParkingLotSelector);
-            SelectParkingLot.SelectByValue("Short");
+            SelectParkingLot.SelectByValue("Long-Surface");
 
             //Entry Date
             IWebElement EntryDate = driver.FindElement(By.Id("StartingDate"));
@@ -93,30 +94,35 @@ namespace Automation_Challenge
             //Leaving Date
             IWebElement LeavingDate = driver.FindElement(By.Id("LeavingDate"));
             LeavingDate.Clear();
-            LeavingDate.SendKeys("4:00");
+            LeavingDate.SendKeys("4/6/2020");
 
             //Leaving Time
-            IWebElement leavingTime = driver.FindElement(By.Id("LeavingTime"));
-            leavingTime.Clear();
-            leavingTime.SendKeys("4:00");
+            IWebElement LeavingTime = driver.FindElement(By.Id("LeavingTime"));
+            LeavingTime.Clear();
+            LeavingTime.SendKeys("5:01");
+
+            //Click Summit
+            IWebElement ClickSummit = driver.FindElement(By.Name("Submit"));
+            ClickSummit.Click();
 
             /*
              Assert
+            $2.00 per hour
+            $10.00 daily maximum
+            $60.00 per week (7th day free)
 
-            Short-Term (hourly) Parking
-            $2.00 first hour; $1.00 each additional 1/2 hour
-            $24.00 daily maximum
-
-            We are looking for 1 day and 4 hours, the result should be $32. The result is the same. 
+            Every day at the time of departure just at 5:01 a.m. to 5:59 a.m. and 
+            also from 6:01a.m. to 6:59 a.m. an additional $2 is charged, 
+            when the maximum that can be charged per day is $10. 
+            The bug is only for the am time frame and not for the pm. 
              
              */
 
             IWebElement ActualResults = driver.FindElement(By.XPath("/html/body/form/table/tbody/tr[4]/td[2]/span[1]/b"));
             var GivenResults = ActualResults.Text;
-            var ExpectedResults = "$ 32.00";
-            Assert.AreEqual(ExpectedResults, ActualResults);
+            var ExpectedResults = "$ 20.00";
+            Assert.AreEqual(GivenResults, ExpectedResults);
            
-
 
         }
     }
