e3f89b23ba5c3840239a756b3f29fef11a8dd24d
Christopher Aguilar <chrisaguilarenriquez@gmail.com>
2020-04-05 13:28:08 -0600

    Update

--- Automation Challenge/Automation Challenge/Program.cs
+++ Automation Challenge/Automation Challenge/Program.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
 
 namespace Automation_Challenge
 {
     class Program
     {
-        static void Main(string[] args)
+        // Reference for Chrome browser
+        IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void Initialize()
         {
-            Console.WriteLine("Hello World!");
+            // Got the website tested
+            driver.Navigate().GoToUrl("http://www.shino.de/parkcalc/");
+        }
+
+
+        [Test]
+        public void ExecuteTest()
+        {
+            //Make browser Full Screen
+            driver.Manage().Window.Maximize();
+
+            //Select Parking Lot
+            IWebElement ParkingLotSelector = driver.FindElement(By.Id("ParkingLot"));
+            var SelectParkingLot = new SelectElement(ParkingLotSelector);
+            SelectParkingLot.SelectByValue("Valet");
+
+            // Entry Date
+            IWebElement EntryDate = driver.FindElement(By.Id("StartingDate"));
+            EntryDate.Clear();
+            EntryDate.SendKeys("4/5/20");
+
+            // Entry Time
+            IWebElement EntryTime = driver.FindElement(By.Id("StartingTime"));
+            EntryTime.Clear();
+            EntryTime.SendKeys("12:00");
+
+            // Leaving Date
+            IWebElement LeavingDate = driver.FindElement(By.Id("LeavingDate"));
+            LeavingDate.Clear();
+            LeavingDate.SendKeys("4/6/2020");
+
+            //Leaving Time
+            IWebElement LeavingTime = driver.FindElement(By.Id("LeavingTime"));
+            LeavingTime.Clear();
+            LeavingTime.SendKeys("4:00");
+
+            //CLick Calcualte
+            IWebElement ClickSummit = driver.FindElement(By.Name("Submit"));
+            ClickSummit.Click();
+
         }
     }
 }
